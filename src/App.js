acf22d299f0c2d37a563b67aa7a131e9d53ffe21
unbearabletruth <antaarty@gmail.com>
2023-06-05 13:54:24 +0700

    router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/home";
+import ShoppingCart from "./components/shoppingCart";
 
-function App() {
-
-}
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/shoppingCart">Shopping Cart</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shoppingCart" element={<ShoppingCart />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/shoppingCart.js
@@ -0,0 +1,12 @@
+const ShoppingCart = () => {
+    return (
+      <>
+        <div>
+            <h1>Hello from shoppingCart</h1>
+        </div>
+        
+      </>
+    );
+  };
+  
+export default ShoppingCart;
